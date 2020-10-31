27fcdfa038d0e8c79655ee547766bfea432e0000
BlaKe00786 <sameershaikh403@gmail.com>
2020-11-01 00:09:24 +0530

    [Sameer]Add: added function to add rides to repo with id and retrive it.

--- CabInvoiceGeneratorProject/Program.cs
+++ CabInvoiceGeneratorProject/Program.cs
@@ -6,7 +6,12 @@ namespace CabInvoiceGeneratorProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            RideRepository rideRepository = new RideRepository();
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 5) };
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            rideRepository.AddRide("sanju", rides);
+            InvoiceSummary invoice = invoiceGenerator.CalculateFare(rideRepository.getRides("sanju"));
+            Console.WriteLine(invoice.averageFare);
         }
     }
 }
